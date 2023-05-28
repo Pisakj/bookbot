880357acc0532c5e254cc0e38e97b738c2c2d9b7
pisakj <varen979@gmail.com>
2023-05-28 23:01:52 +0530

    Add report functionality

--- main.py
+++ main.py
@@ -10,7 +10,20 @@ def count_letters(text):
             fin_dict[ch.lower()] = 1
     return fin_dict
 
+words_count = 0
+letter_statistic = []
+
 with open("books/frankenstein.txt") as f:
     file_contents = f.read()
-    print(read_words(file_contents))
-    print(count_letters(file_contents))
+    words_count = read_words(file_contents)
+    letter_statistic = list(count_letters(file_contents).items())
+
+letter_statistic.sort(key = lambda x:x[1], reverse = True)
+
+print("--- Begin report of books/frankenstein.txt ---")
+print(f"{words_count} words found in the document")
+print("")
+for letter in letter_statistic:
+    if letter[0].isalpha():
+        print(f"The '{letter[0]}' character was found {letter[1]} times")
+print("--- End report ---")
